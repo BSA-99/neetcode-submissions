4ef195a0ed917130b7b04f0484da3276cc5f8175
Bhavpreet Singh Arneja <154618025+BSA-99@users.noreply.github.com>
2026-07-28 13:59:37 -0300

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        new_fleet = list(zip(position, speed)) #[(7,1),(4,2),(1,2),(0,1)]
+        new_fleet.sort(reverse=True)
+        time=[]
+        st = []
+        for i in range(len(new_fleet)):
+             time.append((target-new_fleet[i][0])/new_fleet[i][1])
+        for i in range(len(time)):
+            if not st:
+                st.append(time[i])
+            if time[i]<=st[-1]:
+                continue
+            else:
+                st.append(time[i])
+        return len(st)
+
+            
+
